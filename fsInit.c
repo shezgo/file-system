1f904201523102ec6d2679290aaebebc85b3624b
student <shezpc@gmail.com>
2025-10-19 22:02:16 -0700

    working on cd and fs_setcwd

--- fsInit.c
+++ fsInit.c
@@ -19,6 +19,7 @@ uint8_t magicNumber;
 VolumeControlBlock *vcb = NULL; // Global definition, always kept in memory
 DE *rootGlobal = NULL;			// Global definition, always kept in memory
 DE *cwdGlobal = NULL;			// Global definition, always kept in memory
+char *cwdName = NULL;			// Global char* used to track cwd path string
 Bitmap *bm = NULL;				// Global declaration of the freespace bitmap
 uint8_t *bitmap_bm = NULL;		// Actual bitmap array
 int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
@@ -43,23 +44,43 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 		printf("Loading mounted file system\n");
 		bitmap_bm = loadBMtoMem(blockSize);
 		bm = initBitmap(numberOfBlocks, blockSize, bitmap_bm);
-		
-		printf("bm->mapNumBlocks:%d\n",bm->mapNumBlocks);
+
+		printf("bm->mapNumBlocks:%d\n", bm->mapNumBlocks);
 		rootGlobal = loadDirLBA(vcb->root_num_blocks, vcb->root_directory_block);
 		printf("fsInit RELOAD rootGlobal.dirNumBlocks:%d\n", rootGlobal[0].dirNumBlocks);
-		//Always start cwd from root when starting up file system.
+		// Always start cwd from root when starting up file system.
 		printf("\nfsInit isBitUsed(bm, 12): %d\n\n", isBitUsed(bm, 12));
-		
-		if(rootGlobal == NULL){
+
+		if (rootGlobal == NULL)
+		{
 			fprintf(stderr, "Initializing root failed\n");
 			return -1;
 		}
-		
-		cwdGlobal = rootGlobal; 
-		//Output statement to check if cwdGlobal is initializing to rootGlobal
+
+		cwdGlobal = rootGlobal;
+		// Output statement to check if cwdGlobal is initializing to rootGlobal
 		printf("fsInit.c line 60: cwdGlobal->name: %s\n", cwdGlobal->name);
 		printf("rootGlobal->name: %s\n", rootGlobal->name);
 		// Initialize a global current working directory name string
+
+		cwdName = (char *)malloc(CWD_SIZE);
+
+		if (cwdName == NULL)
+
+		{
+			fprintf(stderr, "cwdName memory allocation failed\n");
+
+			return -1;
+		}
+
+		for (uint32_t i = 0; i < CWD_SIZE; i++)
+
+		{
+
+			cwdName[i] = '\0';
+		}
+
+		strcpy(cwdName, "/");
 		// end init cwdName
 		printf("fsInit.c line 75: cwdName: %s\n", cwdName);
 		return 0;
@@ -77,6 +98,24 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 		((char *)vcb)[i] = 0;
 	}
 
+	// Initialize Global pointer to keep track of current working directory
+
+	cwdName = (char *)malloc(CWD_SIZE);
+
+	if (cwdName == NULL)
+
+	{
+		fprintf(stderr, "cwdName memory allocation failed\n");
+
+		return -1;
+	}
+
+	for (uint32_t i = 0; i < CWD_SIZE; i++)
+
+	{
+		cwdName[i] = '\0';
+	}
+
 	printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks,
 		   blockSize);
 
@@ -85,7 +124,7 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 	vcb->block_size = blockSize;
 	vcb->total_blocks = numberOfBlocks;
 	vcb->free_blocks = vcb->total_blocks - vcb->fsmap_num_blocks - 2; // subtract blocks for bitmap, vcb, root
-	vcb->signature = 0x1A;								  // This is an arbitrary number to check if already initialized
+	vcb->signature = 0x1A;											  // This is an arbitrary number to check if already initialized
 	vcb->fsmap_start_block = 1;
 	vcb->fsmap_end_block = bm->mapNumBlocks;
 	vcb->fsmap_num_blocks = bm->mapNumBlocks;
@@ -97,21 +136,21 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 	printf("fsInit.c: before rootGlobal init\n");
 	rootGlobal = initDir(MAX_ENTRIES, NULL, -1, NULL, bm);
 	printf("fsInit.c: rootGlobal.dirNumBlocks:%d\n", rootGlobal[0].dirNumBlocks);
-	//Current working directory always starts at root when starting file system.
-	cwdGlobal = rootGlobal; 
-	strcpy(cwdName, "/");//Having cwdName is redundant to cwdGlobal
-	
+	// Current working directory always starts at root when starting file system.
+	cwdGlobal = rootGlobal;
+	strcpy(cwdName, "/"); // Having cwdName is redundant to cwdGlobal
+
 	int vcbWriteReturn = LBAwrite(vcb, 1, 0);
-	if(vcbWriteReturn == 1){
+	if (vcbWriteReturn == 1)
+	{
 		setBit(bm, 0);
 	}
-	else{
+	else
+	{
 		fprintf(stderr, "fsInit.c: Failed to LBAwrite VCB\n");
 	}
 
 	printf("sizeof(DE):%ld\n", sizeof(DE));
-
-
 
 	return 0;
 }
